342364c8e4531aff005673813e2977c4a39ca0fc
Rossemary Carballo <144977080+rcarballo08@users.noreply.github.com>
2025-04-02 23:16:58 -0600

    Add files via upload

---
+++ smartdealer/smartdealer/smartdealer/src/main/java/com/smartdealercrm/SmartDealerCrmApplication.java
@@ -0,0 +1,11 @@
+package com.smartdealercrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDealerCrmApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmartDealerCrmApplication.class, args);
+    }
+}
