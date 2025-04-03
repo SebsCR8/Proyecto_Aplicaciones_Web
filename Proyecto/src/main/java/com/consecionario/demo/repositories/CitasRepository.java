e6ad46eb92dccceab9de0b2be22593bd96c84ed1
SebsCR8 <scampos60167@ufide.ac.cr>
2025-04-02 19:49:58 -0600

    Update CitasRepository.java

--- Proyecto/src/main/java/com/consecionario/demo/repositories/CitasRepository.java
+++ Proyecto/src/main/java/com/consecionario/demo/repositories/CitasRepository.java
@@ -10,5 +10,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 public interface CitasRepository extends JpaRepository<Citas, Integer> {
+   
+    @Query("SELECT c FROM Citas c WHERE c.usuario.id = :idUsuario")
+    List<Citas> findByUsuario(@Param("idUsuario") Integer idUsuario);
 
+    
+    @Query("SELECT c FROM Citas c WHERE c.auto.id = :idAuto")
+    List<Citas> findByAuto(@Param("idAuto") Integer idAuto);
+
+    
+    @Query("SELECT c FROM Citas c WHERE c.enganche >= :minEnganche")
+    List<Citas> findByEngancheMayorA(@Param("minEnganche") Double minEnganche);
 }
