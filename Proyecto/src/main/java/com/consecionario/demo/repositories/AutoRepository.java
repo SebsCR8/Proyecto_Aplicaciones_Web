80ffbdd27945493a297854b6e0405afe381259b8
SebsCR8 <scampos60167@ufide.ac.cr>
2025-04-02 20:23:26 -0600

    Update AutoRepository.java

--- Proyecto/src/main/java/com/consecionario/demo/repositories/AutoRepository.java
+++ Proyecto/src/main/java/com/consecionario/demo/repositories/AutoRepository.java
@@ -10,9 +10,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AutoRepository extends JpaRepository<Auto, Long> {
-    @Query("SELECT a FROM Auto a WHERE a.tipo = :tipo AND a.marca.nombreMarca = :marca AND a.precio <= :precioMax")
-    List<Auto> filtrarVehiculos(@Param("tipo") String tipo,
-                                @Param("marca") String marca,
-                                @Param("precioMax") Double precioMax);
+   @Query("SELECT a FROM Auto a WHERE a.tipo = :tipo AND a.marca.nombreMarca = :marca AND a.precio <= :precioMax")
+List<Auto> filtrarVehiculos(@Param("tipo") String tipo,
+                            @Param("marca") String marca,
+                            @Param("precioMax") Double precioMax);
 }
 
