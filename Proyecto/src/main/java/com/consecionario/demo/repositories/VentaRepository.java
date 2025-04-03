d10245e40f9a482dae5e7dbd9d1918f0b7086683
SebsCR8 <scampos60167@ufide.ac.cr>
2025-04-02 20:39:10 -0600

    Update VentaRepository.java

--- Proyecto/src/main/java/com/consecionario/demo/repositories/VentaRepository.java
+++ Proyecto/src/main/java/com/consecionario/demo/repositories/VentaRepository.java
@@ -5,10 +5,23 @@ package com.consecionario.demo.repositories;
  * @author sebas
  */
 import com.consecionario.demo.domain.Venta;
+import java.util.Date;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VentaRepository extends JpaRepository<Venta, Long> {
-    // Aqui pueden ir metodos personalizados
+public interface VentaRepository extends JpaRepository<Venta, Integer> {
+
+    // Buscar ventas por ID de usuario
+    List<Venta> findByIdUsuario(Integer idUsuario);
+
+    // Buscar ventas por ID de auto
+    List<Venta> findByIdAuto(Integer idAuto);
+
+    // Buscar ventas por fecha
+    List<Venta> findByFechaVenta(Date fecha);
+
+    // Buscar ventas entre dos fechas
+    List<Venta> findByFechaVentaBetween(Date desde, Date hasta);
 }
