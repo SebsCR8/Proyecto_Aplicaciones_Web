aa078d3418a97013e567fd0f4b82e55589eca154
SebsCR8 <scampos60167@ufide.ac.cr>
2025-04-02 20:32:34 -0600

    Update CitasRepository.java

--- Proyecto/src/main/java/com/consecionario/demo/repositories/CitasRepository.java
+++ Proyecto/src/main/java/com/consecionario/demo/repositories/CitasRepository.java
@@ -10,5 +10,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 public interface CitasRepository extends JpaRepository<Citas, Integer> {
+    // Consulta para obtener citas con datos completos del usuario y auto
+    @Query("SELECT c FROM Citas c JOIN FETCH c.usuario JOIN FETCH c.auto")
+    List<Citas> obtenerCitasConDetalles();
+
+    
+    List<Citas> findByUsuarioId(Integer idUsuario);
+    List<Citas> findByAutoId(Integer idAuto);
 
 }
