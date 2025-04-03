1850bd66546bb2fd8e0dea98c398ab8afeb8c765
SebsCR8 <scampos60167@ufide.ac.cr>
2025-04-02 19:36:54 -0600

    AutoRepository.java
    Insercion de las consultas

--- Proyecto/src/main/java/com/consecionario/demo/repositories/AutoRepository.java
+++ Proyecto/src/main/java/com/consecionario/demo/repositories/AutoRepository.java
@@ -10,6 +10,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AutoRepository extends JpaRepository<Auto, Long> {
-    // Aqui pueden ir metodos personalizados
+    @Query("SELECT a FROM Auto a WHERE a.tipo = :tipo AND a.marca.nombreMarca = :marca AND a.precio <= :precioMax")
+    List<Auto> filtrarVehiculos(@Param("tipo") String tipo,
+                                @Param("marca") String marca,
+                                @Param("precioMax") Double precioMax);
 }
 
